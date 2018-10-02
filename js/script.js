97fa08739e5e1cab5c63b13d48af0908a1ebdc4f
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-02 14:46:27 -0300

    Ajustar estilo da mensagem de sucesso

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@ const input = document.querySelector(".news__form");
 const button = document.querySelector(".news__button");
 const email = document.querySelector("#newsInputEmail");
 button.addEventListener("click", function(event) {
-    event.preventDefault();
+    event.preventDefault(); //não zerar o formulário
+    // condicional para validar email
     if(email.length < 1 || email.value.indexOf("@")==-1 || email.value.indexOf(".")==-1) {
         alert("não é um email válido");
         return false;
     } else {
+        //mostrar mensagem de sucesso
         const success = document.createElement("span");
-    const successText = document.createTextNode("E-mail cadastrado com sucesso!");
-    success.appendChild(successText);
-    input.parentNode.insertBefore(success, input);
+        const successText = document.createTextNode("E-mail cadastrado com sucesso!");
+        success.appendChild(successText);
+        input.parentNode.insertBefore(success, input);
     }
-    button.disabled = true;
+    button.disabled = true; //desabilitar o botão
 });
 
 
