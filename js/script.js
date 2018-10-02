ec74cf94f6ed2342a41007398ce6ab8ca020ab13
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-01 22:13:54 -0300

    Validar e-mail

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const input = document.querySelector(".news__form");
 const button = document.querySelector(".news__button");
+const email = document.querySelector("#newsInputEmail");
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    const success = document.createElement("span");
+    if(email.length < 1 || email.value.indexOf("@")==-1 || email.value.indexOf(".")==-1) {
+        alert("não é um email válido");
+        return false;
+    } else {
+        const success = document.createElement("span");
     const successText = document.createTextNode("E-mail cadastrado com sucesso!");
     success.appendChild(successText);
     input.parentNode.insertBefore(success, input);
-})
+    }
+    button.disabled = true;
+});
+
+
+
